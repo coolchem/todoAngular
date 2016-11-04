e3fd539d6d5a16f0768de20631801333db566ba3
coolchem <varun8686@gmail.com>
2016-11-04 07:41:50 -0600

    fixing e2e tests..

--- test/e2e/app.e2e.ts
+++ test/e2e/app.e2e.ts
@@ -13,19 +13,10 @@ describe('App', () => {
     expect(subject).toEqual(result);
   });
 
-  it('should have app div', () => {
-    let subject = element(by.id('app')).isPresent();
+  it('should have app element', () => {
+    let subject = element(by.tagName('my-app')).isPresent();
     let result  = true;
     expect(subject).toEqual(result);
   });
 
-  it('should say hi', ()=>{
-
-    let sayHiButton = element(by.id('sayHiButton'));
-    let sayHiResponseLabel  = element(by.id('sayHiResponseLabel'));
-
-    sayHiButton.click();
-
-    expect(sayHiResponseLabel.getText()).toEqual("hello hi")
-  })
 });
